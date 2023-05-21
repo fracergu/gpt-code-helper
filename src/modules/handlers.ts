0533df21c78fde7996103d30cdcdc43920633660
Francisco Cerdán <fcerdanguerrero@protonmail.com>
2023-05-21 15:32:57 +0200

    Added insertAtCursor function and configured when pasting from clipboard

--- src/modules/handlers.ts
+++ src/modules/handlers.ts
@@ -19,34 +19,35 @@ export const setupHandlers = () => {
       textarea.removeEventListener(type, handler)
     }
   }
+  const insertAtCursor = (textarea: HTMLTextAreaElement, text: string) => {
+    const cursorPosition = textarea.selectionStart
+    const textBeforeCursor = textarea.value.substring(0, cursorPosition)
+    const textAfterCursor = textarea.value.substring(cursorPosition, textarea.value.length)
+
+    textarea.value = textBeforeCursor + text + textAfterCursor
+    textarea.selectionStart = cursorPosition + text.length
+    textarea.selectionEnd = cursorPosition + text.length
+    textarea.style.height = 'auto'
+  }
 
   const enterHandler = (e: KeyboardEvent) => {
     if (e.key === 'Enter') {
       e.preventDefault()
       e.stopPropagation()
 
-      const cursorPosition = textarea.selectionStart
-      const textBeforeCursor = textarea.value.substring(0, cursorPosition)
-      const textAfterCursor = textarea.value.substring(cursorPosition, textarea.value.length)
-
-      textarea.value = textBeforeCursor + '\n' + textAfterCursor
-
-      textarea.selectionStart = cursorPosition + 1
-      textarea.selectionEnd = cursorPosition + 1
-
-      textarea.style.height = 'auto'
-      textarea.style.height = `${textarea.scrollHeight}px`
+      insertAtCursor(textarea, '\n')
     }
   }
 
   const pasteHandler = (e: ClipboardEvent) => {
     e.preventDefault()
     e.stopPropagation()
+
     if (e.clipboardData == null) return
     const text = e.clipboardData.getData('text/plain')
-    textarea.value += minimizeCode(text)
-    textarea.style.height = '1px'
-    textarea.selectionStart = textarea.value.length
+    const minifiedText = minimizeCode(text)
+
+    insertAtCursor(textarea, minifiedText)
   }
 
   manageListeners('keydown', disableEnter, enterHandler)
